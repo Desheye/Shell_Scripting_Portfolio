ab066d0eb4a45af78b43c6006ddb5d33f3805932
Desheye <aarealaafiaboyz@gmail.com>
2023-11-15 18:24:48 +0100

    simple shell

---
+++ simple_shell/README.md
@@ -0,0 +1,2 @@
+Hi, my name is Desheye and im going to attempt this shell task a third time,
+wish me luck.
---
+++ simple_shell/mem.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+int freeAndNull(void **ptr)
+{
+    if (ptr && *ptr)
+    {
+        free(*ptr);
+        *ptr = NULL;
+        return 1;
+    }
+
+    return 0;
+}
+
+
